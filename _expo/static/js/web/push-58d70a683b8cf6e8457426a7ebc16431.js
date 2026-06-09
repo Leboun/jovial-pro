9e3f65d7f80efb0a72161e5565682fe5828c381a
Esteban <vialetteesteban@gmail.com>
2026-06-09 21:36:16 +0200

    Login mascotte sans cadre + email oublie, sidebar logo adaptatif + filigrane Jovial + deconnexion lisible

--- _expo/static/js/web/push-58d70a683b8cf6e8457426a7ebc16431.js
+++ _expo/static/js/web/push-58d70a683b8cf6e8457426a7ebc16431.js
@@ -1,3 +1,3 @@
-﻿__d(function(g,r,i,a,m,e,d){"use strict";Object.defineProperty(e,'__esModule',{value:!0}),e.registerPushToken=async function(n){return},r(d[0]);r(d[1]),r(d[2])},1228,{"0":123,"1":755,"2":1213,"3":571,"4":827,"5":927,"paths":{}});
+__d(function(g,r,i,a,m,e,d){"use strict";Object.defineProperty(e,'__esModule',{value:!0}),e.registerPushToken=async function(n){return},r(d[0]);r(d[1]),r(d[2])},1228,{"0":123,"1":755,"2":1213,"3":571,"4":827,"5":927,"paths":{}});
 //# sourceMappingURL=/_expo/static/js/web/push-58d70a683b8cf6e8457426a7ebc16431.js.map
 //# debugId=16749e6f-b4f3-4c94-9d2a-ecb89b606c61
